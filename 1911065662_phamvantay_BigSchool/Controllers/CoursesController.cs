8037582005882f47444576c851874e8a8de853c2
84347 <84347@LAPTOP-C95ULGJ0>
2022-03-20 17:08:27 +0700

    commit5

--- 1911065662_phamvantay_BigSchool/Controllers/CoursesController.cs
+++ 1911065662_phamvantay_BigSchool/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using _1911065662_phamvantay_BigSchool.Models;
 using _1911065662_phamvantay_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Web.Mvc;
 
 namespace _1911065662_phamvantay_BigSchool.Controllers
 {
-    //adada
+
     public class CoursesController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
@@ -24,6 +25,28 @@ namespace _1911065662_phamvantay_BigSchool.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create",viewModel);
+            }
+
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
