ba4f8d2d4970ed429d6cc28c92ae5c53cc3def6e
84347 <84347@LAPTOP-C95ULGJ0>
2022-03-21 16:31:00 +0700

    commit: Nộp bài lúc 16h30 ngày 21/03/2022

---
+++ 1911065662_phamvantay_BigSchool/Models/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _1911065662_phamvantay_BigSchool.Models
+{
+    //public class RegisterViewModel
+    //{
+    //    [Required]
+    //    [EmailAddress]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
+    //    [Required]
+    //    [StringLength(100, ErrorMessage = "The {e} must be at
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Password")]
+    //    public string Password { get; set; }
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Confirm password")]
+    //    [Compare("Password", ErrorMessage = "The password and
+    //    public string ConfirmPassword { get; set; }
+    //    [Required]
+    //    [StringLength(255)]
+    //    public string Name { get; set; }
+    //}
+}
